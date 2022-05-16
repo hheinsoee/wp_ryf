8f632186833b4570f4441ad9a6f263e81742c75f
Hein Soe <hheinsoee@gmail.com>
2022-05-16 18:51:58 +0630

    theme change and Shop banner add

--- footer.php
+++ footer.php
@@ -1,28 +1,27 @@
-<div class="shop bg-dark text-light" style="height: 300px;display:flex;justify-content:center;align-items:center">
+<div class="shop_banner bg-dark text-light" style="height: 300px;display:flex;justify-content:center;align-items:center">
     <center>
         <span class="h2">Shop</span> is
         <div>comming soon</div>
     </center>
 </div>
-<footer class="mt-5" >
-    <div class="card py-5 px-2">
-        <center>main sponsor</center>
-        <div class="d-flex justify-content-center">
-            Sittwe Gas
-        </div>
-        <div class="d-flex justify-content-center">
-            Sann Thit Thazin Aung Tha Hla
-        </div>
-        <div class="d-flex justify-content-center">
-           Arakan.LIVE
+<footer class="mt-5">
+    <div class="py-5 px-2">
+        <center>main sponsored by</center>
+        <div class="p m">
+            <div class="d-flex justify-content-center">
+                <img class="logo_size mx-3 p-1" src="<?php echo get_template_directory_uri(); ?>/assets/img/sponsor/tz.png" alt="">
+                <img class="logo_size mx-3 p-1" src="<?php echo get_template_directory_uri(); ?>/assets/img/sponsor/tz.png" alt="">
+                <img class="logo_size mx-3 p-1" src="<?php echo get_template_directory_uri(); ?>/assets/img/sponsor/tz.png" alt="">
+                <img class="logo_size mx-3 p-1" src="<?php echo get_template_directory_uri(); ?>/assets/img/sponsor/tz.png" alt="">
+            </div>
         </div>
     </div>
-    <center class="bg-primary text-white">website is sponsored by www.heinsoe.com </center>
+    <center><small><a href="https://www.heinsoe.com" target="_blank">website is sponsored by www.heinsoe.com</a></small></center>
     <div class="text-light bg-dark">
         <div class="d-md-flex d-lg-flex justify-content-center py-5 px-2 h3">
-        <div class="m-3"><a class="bi bi-facebook p" href="https://fb.com/rakhineyouthfutsal">&nbsp;Facebook</a></div>
-        <div class="m-3"><a class="bi bi-messenger p" href="https://m.me/rakhineyouthfutsal">&nbsp;Messenger</a></div>
-        <div class="m-3"><a class="bi bi-youtube p" href="https://www.youtube.com/channel/UCGQNfG6mK_QEElIcznSjhyw">&nbsp;Youtube</a></div>
+            <div class="m-3"><a class="bi bi-facebook p" href="https://fb.com/rakhineyouthfutsal">&nbsp;Facebook</a></div>
+            <div class="m-3"><a class="bi bi-messenger p" href="https://m.me/rakhineyouthfutsal">&nbsp;Messenger</a></div>
+            <div class="m-3"><a class="bi bi-youtube p" href="https://www.youtube.com/channel/UCGQNfG6mK_QEElIcznSjhyw">&nbsp;Youtube</a></div>
         </div>
         <hr />
         <small class="d-flex flex-wrap justify-content-between px-3 pb-3">
@@ -45,7 +44,7 @@
                 ?>
             </div>
             <div>
-                &copy;&nbsp;<?php echo date('Y')." ".get_bloginfo('name');?>
+                &copy;&nbsp;<?php echo date('Y') . " " . get_bloginfo('name'); ?>
             </div>
         </small>
     </div>

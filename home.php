2c036e94b631d96bb4d16fcb6f8a0f5f5b942180
Hein Soe <hheinsoee@gmail.com>
2022-05-17 23:12:24 +0630

    HOme Pag Add

--- home.php
+++ home.php
@@ -1,38 +1,8 @@
 <?php
 get_header();
+include_once __DIR__ . '/include/slider.php';
+
 ?>
-<div class="container">
-    <div class="my-2 row g-lg-4 g-md-3 g-2 row-cols-1 row-cols-md-2 ">
-        <?php
-        $args = array(
-            'posts_per_page' => 2,
-            'paged' => 1,
-            'offset' => 0,
-            // 'post_type' => 'my_custom_type'
-            // 'tax_query' => array(
-            //     array(
-            //         'taxonomy' => 'category', //double check your taxonomy name in you dd 
-            //         'field'    => 'id',
-            //         'terms'    => $cat_id,
-            //     ),
-            //    ),
-        );
-        $wp_query = new WP_query($args);
-
-        if ($wp_query->have_posts()) {
-            while ($wp_query->have_posts()) {
-                $wp_query->the_post();
-        ?>
-                <div class="col"><?php get_template_part('thumbnail', 'feature'); ?></div>
-        <?php
-            }
-        } else {
-            // no posts found
-        }
-        ?>
-    </div>
-
-</div>
 
 <div class="container">
     <div class="
@@ -47,9 +17,9 @@ get_header();
     row-cols-lg-4">
         <?php
         $args_2 = array(
-            'posts_per_page' => 8,
+            'posts_per_page' => 4,
             'paged' => 1,
-            'offset' => 2,
+            'offset' => 0,
             // 'post_type' => 'my_custom_type'
             // 'tax_query' => array(
             //     array(
